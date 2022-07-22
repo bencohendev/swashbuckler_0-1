360b2c513d496d8a80a173d299fd27e9f08e9e21
bencohendev <ben@bencohen.dev>
2022-07-22 15:57:49 -0400

    sass

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,5 +2,13 @@
 	import Auth from '$c/Auth.svelte';
 </script>
 
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<h1 class="ben text-3xl font-bold underline"><div>Hello world!</div></h1>
 <Auth />
+
+<style lang="scss">
+	.ben {
+		div {
+			color: red;
+		}
+	}
+</style>
